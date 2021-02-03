63f9a775d2543449ea29c675177a18578099556a
Edward Not <leonardovan@yandex.ru>
2021-02-03 14:21:03 +0300

    Update status of task on server

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
       <h3>ToDos</h3>
+      <div class = "legend">
+        <span>Double click to mark as comlete  </span>
+        <span>
+          <span class ="incomplete-box"></span> = Incomplete
+        </span>
+        <span>
+          <span class ="complete-box"></span> = Complete
+        </span>
+      </div>
       <div class="todos">
-        <div class="todo" v-for="todo in allTodos" :key="todo.id">
+        <div 
+          @dblclick="onDblClick(todo)"
+          class="todo"
+          v-for="todo in allTodos"
+          :key="todo.id"
+          v-bind:class="{'is-complete':todo.completed}"
+        >
           {{todo.title}}
           <i @click="deleteTodo(todo.id)" class = "fas fa-trash-alt"></i>
         </div>
@@ -17,7 +32,16 @@ import { mapGetters, mapActions } from "vuex"
 export default {
     name: "Todos",
     methods: {
-      ...mapActions(['fetchTodos', 'deleteTodo'])
+      ...mapActions(['fetchTodos', 'deleteTodo', "updateTodo"]),
+      onDblClick(todo) {
+        const updtTodo = {
+          id: todo.id,
+          title: todo.title,
+          completed: !todo.completed
+        }
+
+        this.updateTodo(updtTodo);
+      }
     },
     computed: mapGetters(['allTodos']),
     created() {
@@ -49,5 +73,24 @@ export default {
     right: 10px;
     color: #fff;
     cursor: pointer;
+  }
+
+  .complete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background-color: #35495e;
+  }
+
+  .is-complete {
+    background: #35495e;
+    color: white;
+  }
+
+  .incomplete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background-color: #41b883;
   }
 </style>
